9a6c63ffb9194f62e87e4cf0e79b897ffc38c849
James Abney <james.abney@gmail.com>
2020-02-01 10:37:56 -0800

    Add env var import

--- src/config/environment.js
+++ src/config/environment.js
@@ -4,9 +4,12 @@ import {
   // @ts-ignore
   API_KEY as _API_KEY,
   // @ts-ignore
+  TOKEN_WILL_EXPIRE_HOURS as _TOKEN_WILL_EXPIRE_HOURS,
+  // @ts-ignore
   DEBUG as _DEBUG,
 } from 'react-native-dotenv'
 
 export const API_KEY = _API_KEY
 export const API_URL = _API_URL
+export const TOKEN_WILL_EXPIRE_HOURS = _TOKEN_WILL_EXPIRE_HOURS
 export const DEBUG = typeof _DEBUG === 'string' && _DEBUG.trim().toLowerCase() === 'true'
