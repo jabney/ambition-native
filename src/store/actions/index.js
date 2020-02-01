6328e271020b5cb39d00bb21863337a6c45d79fc
James Abney <james.abney@gmail.com>
2020-02-01 13:23:04 -0800

    Implement init, signin, and signup actions

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,3 +1,4 @@
+import api from 'src/services/api.service'
 
 /**
  * @template T
@@ -5,18 +6,19 @@
  */
 
 /**
- * @typedef {import('../../models/store').IStore} Store
+ * @typedef {import('src/models/store').IStore} Store
  * @typedef {import('src/models/user').IUser} User
+ * @typedef {import('src/models/user').ICredentials} Credentials
  */
 
 /**
- * @template A
- * @typedef {import('redux-thunk').ThunkAction<void, Store, void, A>} ThunkAction
+ * @template T
+ * @typedef {import('redux-thunk').ThunkAction<void, Store, void, Action<T>>} ThunkAction
  */
 
 /**
- * @template A
- * @typedef {import('redux-thunk').ThunkDispatch<Store, void, A>} ThunkDispatch
+ * @template T
+ * @typedef {import('redux-thunk').ThunkDispatch<Store, void, Action<T>>} ThunkDispatch
  */
 
 export const SET_USER = 'set-user'
@@ -26,4 +28,69 @@ export const SET_USER = 'set-user'
  */
 export const setUser = (user) => {
   return { type: SET_USER, payload: user }
+}
+
+/**
+ * @param {Credentials} cred
+ *
+ * @returns {ThunkAction<User>}
+ */
+export const signup = (cred) => async (dispatch) => {
+  const [signupErr] = await api.signup(cred)
+
+  if (signupErr) {
+    return console.error(signupErr)
+  }
+
+  const [userErr, user] = await api.user()
+
+  if (userErr) {
+    return console.error(userErr)
+  }
+
+  dispatch({ type: SET_USER, payload: user })
+}
+
+/**
+ * @param {Credentials} cred
+ *
+ * @returns {ThunkAction<User>}
+ */
+export const signin = (cred) => async (dispatch) => {
+  const [signinErr] = await api.signin(cred)
+
+  if (signinErr) {
+    return console.error(signinErr)
+  }
+
+  const [userErr, user] = await api.user()
+
+  if (userErr) {
+    return console.error(userErr)
+  }
+
+  dispatch({ type: SET_USER, payload: user })
+}
+
+/**
+ * Initialize the store.
+ */
+export const init = () => async (dispatch) => {
+  const [tokenErr, tokenIsValid] = await api.tokenIsValid()
+
+  if (tokenErr) {
+    return console.error(tokenErr)
+  }
+
+  if (!tokenIsValid) {
+    return dispatch ({ type: SET_USER, payload: {} })
+  }
+
+  const [userErr, user] = await api.user()
+
+  if (userErr) {
+    return console.error(userErr)
+  }
+
+  dispatch({ type: SET_USER, payload: user })
 }
