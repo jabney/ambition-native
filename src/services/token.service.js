a9d92750b10f972d131d9a7ca0ba4ec38b9ba925
James Abney <james.abney@gmail.com>
2020-02-01 16:13:09 -0800

    Fix token clear method deleting all settings

--- src/services/token.service.js
+++ src/services/token.service.js
@@ -5,7 +5,7 @@ const TOKEN_KEY = 'access-token'
 class TokenStorage {
   get = () => AsyncStorage.getItem(TOKEN_KEY)
   set = (token) => AsyncStorage.setItem(TOKEN_KEY, token)
-  clear = () => AsyncStorage.clear()
+  clear = () => AsyncStorage.removeItem(TOKEN_KEY)
 }
 
 export default Object.freeze(new TokenStorage())
