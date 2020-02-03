ff0a02057846684941b31562674c5d7ffcef4321
James Abney <james.abney@gmail.com>
2020-02-02 19:45:30 -0800

    Configure input props for email field

--- src/components/auth-form/auth-form.component.js
+++ src/components/auth-form/auth-form.component.js
@@ -23,7 +23,7 @@ const AuthForm = ({ model, onChange, buttonText, linkText, onButton, onLink, ani
   const disabled = email.length === 0 || password.length === 0
 
   return <View style={styles.container}>
-    <SlickInput value={email} label='Email' containerStyle={anims.email} inputStyle={styles.input} onChangeText={setEmail} autoCapitalize='none' autoCorrect={false} />
+    <SlickInput value={email} label='Email' containerStyle={anims.email} inputStyle={styles.input} onChangeText={setEmail} autoCapitalize='none' autoCorrect={false} autoCompleteType='email' keyboardType='email-address' textContentType='emailAddress' />
     <SlickInput value={password} label='Password' containerStyle={anims.password} inputStyle={styles.input} onChangeText={setPassword} secureTextEntry={true} />
     <Button title={buttonText} containerStyle={[styles.button, anims.button]} onPress={onButton} disabled={busy || disabled} busy={busy} />
     <NavLink text={linkText} containerStyle={[styles.link, anims.link]} onPress={onLink} />
